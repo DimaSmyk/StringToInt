22f3aad2403f7a3ecb04d575f26ca22658abb0d9
DimaSmyk <dima.smyk99.19@gmail.com>
2021-09-13 18:13:20 +0300

    1

--- StringToInt/src/transformation/ConvertStringToInt.java
+++ StringToInt/src/transformation/ConvertStringToInt.java
@@ -1,38 +1,30 @@
 package transformation;
 
+import java.util.Scanner;
+
 public class ConvertStringToInt {
 
     static int num;
     public static void main(String[] args) {
-
-        String input = "";
-        if(input.length() >= 2 && input.length() <= Math.pow(2,32) ){
-            stringToInt(input);
-        }
-
-
+        scScanner();
     }
-
     public static void stringToInt(String input) throws NumberFormatException {
 
-        if (input.isEmpty()) {
-            throw new NumberFormatException("null");
-        }
         char firstChar = input.charAt(0);
         if (firstChar == '+' | firstChar == '-') {
             transformationCycle(1, input);
             if (firstChar == '+') {
-                System.out.println(num);
+                System.out.println(num );
             } else {
-                System.out.println("-" + num);
+                System.out.println( "-" + num);
             }
         } else {
             transformationCycle(0, input);
-            System.out.println(num);
+            System.out.println( num);
         }
     }
 
-    public static int transformationCycle(int a, String input) {
+    public static void transformationCycle(int a, String input) {
 
         for (int i= a; i < input.length(); i++) {
             if ((input.charAt(i) >= 48) && (input.charAt(i) <= 59)) {
@@ -41,6 +33,25 @@ public class ConvertStringToInt {
                 throw new NumberFormatException();
             }
         }
-        return num;
     }
+
+    public static void scScanner() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        String input = sc.nextLine();
+        double maxLimit = Math.pow(2,32) - 1;
+
+        while (true) {
+            if (input.length() >= 2 && input.length() <= maxLimit || input.equals("0")) {
+                stringToInt(input);
+                break;
+            }
+            else {
+                System.out.println("If you need to enter numbers [1-9]. Add '+' or '-' at the beginning");
+                System.out.println("Repeat enter");
+                input = sc.nextLine();
+            }
+        }
+    }
+
 }
